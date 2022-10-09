a868d33f724c7aae64a8c8ede1c726f9ea1d2d75
MerJP <merjuradoperez@gmail.com>
2022-10-09 06:32:26 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola mundo, comienza la nueva app, y con ello el fin del bootcamp!!!!</h1>
+      <s style={{textDecoration:'none'}}>Happy codding!</s>
+    </div>
+  );
+}
+
+export default App;
