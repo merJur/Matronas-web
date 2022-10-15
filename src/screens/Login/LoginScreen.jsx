52751a544ae0ce377bfa3954969aa02ef58c9441
MerJP <merjuradoperez@gmail.com>
2022-10-15 13:42:27 +0200

    witg google added

--- src/screens/Login/LoginScreen.jsx
+++ src/screens/Login/LoginScreen.jsx
@@ -1,71 +1,96 @@
-import React from 'react';
-import('./LoginScreen.css');
+import React from "react";
+import("./LoginScreen.css");
 
 const LoginScreen = () => {
-    const handleChange = (event) => {}
-    return (
-        <div>
-            <section className="vh-100" style={{backgroundColor: "#eee"}}>
-              <div className="container h-100">
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                  <div className="col-lg-12 col-xl-11">
-                    <div className="card text-black" style={{borderRadius: '25px'}}>
-                      <div className="card-body p-md-5">
-                        <div className="row justify-content-center">
-                          <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                
-                            <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Login</p>
-                
-                            <form className="mx-1 mx-md-4">
-                                                             
-                              <div className="d-flex flex-row align-items-center mb-4">
-                                <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                                <div className="form-outline flex-fill mb-0">
-                                  <input 
-                                  type="email" 
-                                  name="email"
-                                  placeholder="e-mail"
-                                  className="form-control" 
-                                  onChange={(event) => handleChange(event)} />
-                                  <label className="form-label" htmlFor='form3Example3c'>Your Email</label>
-                                </div>
-                              </div>
-                
-                              <div className="d-flex flex-row align-items-center mb-4">
-                                <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                                <div className="form-outline flex-fill mb-0">
-                                  <input 
-                                  type="password" 
-                                  name="password"
-                                  placeholder="password"
-                                  className="form-control" 
-                                  onChange={(event) => handleChange(event)} />
-                                  <label className="form-label" htmlFor='form3Example4c'>Password</label>
-                                </div>
-                              </div>
-                                          
-                              <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                <button type="button" className="btn btn-primary btn-lg">Login</button>
-                              </div>
-                
-                            </form>
-                
-                          </div>
-                          <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                
-                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                              className="img-fluid" alt="Register img" />
-                            
+  const handleChange = (event) => {};
+  return (
+    <div>
+      <section className="vh-100" style={{ backgroundColor: "#eee" }}>
+        <div className="container h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-lg-12 col-xl-11">
+              <div className="card text-black" style={{ borderRadius: "25px" }}>
+                <div className="card-body p-md-5">
+                  <div className="row justify-content-center">
+                    <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                      <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                        Login
+                      </p>
+
+                      <form className="mx-1 mx-md-4">
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <input
+                              type="email"
+                              name="email"
+                              placeholder="e-mail"
+                              className="form-control"
+                              onChange={(event) => handleChange(event)}
+                            />
+                            <label
+                              className="form-label"
+                              htmlFor="form3Example3c"
+                            >
+                              Your Email
+                            </label>
                           </div>
                         </div>
-                      </div>
+
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                          <div className="form-outline flex-fill mb-0">
+                            <input
+                              type="password"
+                              name="password"
+                              placeholder="password"
+                              className="form-control"
+                              onChange={(event) => handleChange(event)}
+                            />
+                            <label
+                              className="form-label"
+                              htmlFor="form3Example4c"
+                            >
+                              Password
+                            </label>
+                          </div>
+                        </div>
+                        <div className="d-grid gap-2 mt-2">
+                          <a
+                            href="/login/google"
+                            class="btn btn-danger border-0"
+                          >
+                            <i className="fa fa-google"></i>
+                            Inicia sesión con Google
+                          </a>
+                        </div>
+
+                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                          <button
+                            type="button"
+                            className="btn btn-primary btn-lg"
+                          >
+                            Login
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                    <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                      <img
+                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                        className="img-fluid"
+                        alt="Register img"
+                      />
                     </div>
                   </div>
                 </div>
               </div>
-            </section>
+            </div>
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default LoginScreen;
