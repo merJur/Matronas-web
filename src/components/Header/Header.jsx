e3b634bd99724b61f91501ba862a368bf1371793
idoiafforero <idoia.forero@gmail.com>
2022-10-22 14:32:58 +0200

    Header actualizado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <div>
         <nav className="navbar bg-light fixed-top">
   <div className="container-fluid">
-    <Link className="navbar-brand" href="#">OM Matronas</Link>
+    <Link className="navbar-brand" to="#">OM Matronas</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -19,76 +19,76 @@ const Header = () => {
       <div className="offcanvas-body">
         <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" href="#">Inicio</Link>
+            <Link className="nav-link active" aria-current="page" to="#">Inicio</Link>
           </li>
           <li className="nav-item dropdown">
-            <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Sobre Nosotras
             </Link>
             <ul className="dropdown-menu">
-              <li><Link className="dropdown-item" href="#">Rosa Mª Sánchez Molina</Link></li>
-              <li><Link className="dropdown-item" href="#">Elena Rivilla Lizano</Link></li>
+              <li><Link className="dropdown-item" to="#">Elena Rivilla Lizano</Link></li>
+              <li><Link className="dropdown-item" to="#">Rosa Mª Sánchez Molina</Link></li>
             </ul>
           </li>
           <li className="nav-item dropdown">
-            <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Niña y Adolescente
             </Link>
             <ul className="dropdown-menu">
-              <li><Link className="dropdown-item" href="#">Taller de menarquia</Link></li>
-              <li><Link className="dropdown-item" href="#">Sexualidad en la adolescencia</Link></li>
-              <li><Link className="dropdown-item" href="#">Sexualidad en la adolescencia</Link></li>
+              <li><Link className="dropdown-item" to="#">Taller de menarquia</Link></li>
+              <li><Link className="dropdown-item" to="#">Sexualidad en la adolescencia</Link></li>
+              <li><Link className="dropdown-item" to="#">Sexualidad en la adolescencia</Link></li>
             </ul>
           </li>
           <li className="nav-item dropdown">
-            <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Mujer
             </Link>
             <ul className="dropdown-menu">
-              <li><Link className="dropdown-item" href="#">Yoga Mujer</Link></li>
-              <li><Link className="dropdown-item" href="#">Sexualidad saludable</Link></li>
-              <li><Link className="dropdown-item" href="#">Fertilidad</Link></li>
+              <li><Link className="dropdown-item" to="#">Yoga Mujer</Link></li>
+              <li><Link className="dropdown-item" to="#">Sexualidad saludable</Link></li>
+              <li><Link className="dropdown-item" to="#">Fertilidad</Link></li>
             </ul>
           </li>
           <li className="nav-item dropdown">
-            <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Embarazo
             </Link>
             <ul className="dropdown-menu">
-              <li><Link className="dropdown-item" href="#">Yoga Prenatal</Link></li>
-              <li><Link className="dropdown-item" href="#">Esferodinamia e Hipnoparto</Link></li>
-              <li><Link className="dropdown-item" href="#">Curso Preparación al parto</Link></li>
-              <li><Link className="dropdown-item" href="#">Taller de Lactancia Materna</Link></li>
+              <li><Link className="dropdown-item" to="#">Yoga Prenatal</Link></li>
+              <li><Link className="dropdown-item" to="#">Esferodinamia e Hipnoparto</Link></li>
+              <li><Link className="dropdown-item" to="#">Curso Preparación al parto</Link></li>
+              <li><Link className="dropdown-item" to="#">Taller de Lactancia Materna</Link></li>
             </ul>
           </li>
           <li className="nav-item dropdown">
-            <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Postparto
             </Link>
             <ul className="dropdown-menu">
-              <li><Link className="dropdown-item" href="#">Yoga madres y bebés</Link></li>
-              <li><Link className="dropdown-item" href="#">Taller creación banco de leche e incorporación al trabajo.</Link></li>
-              <li><Link className="dropdown-item" href="#">Sexualidad tras la cuarentena</Link></li>
+              <li><Link className="dropdown-item" to="#">Yoga madres y bebés</Link></li>
+              <li><Link className="dropdown-item" to="#">Taller creación banco de leche e incorporación al trabajo.</Link></li>
+              <li><Link className="dropdown-item" to="#">Sexualidad tras la cuarentena</Link></li>
             </ul>
           </li>
           <li className="nav-item dropdown">
-            <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Crianza
             </Link>
             <ul className="dropdown-menu">
-              <li><Link className="dropdown-item" href="#">Yoga Mujer</Link></li>
-              <li><Link className="dropdown-item" href="#">Sexualidad saludable</Link></li>
-              <li><Link className="dropdown-item" href="#">Fertilidad</Link></li>
+              <li><Link className="dropdown-item" to="#">Yoga Mujer</Link></li>
+              <li><Link className="dropdown-item" to="#">Sexualidad saludable</Link></li>
+              <li><Link className="dropdown-item" to="#">Fertilidad</Link></li>
             </ul>
           </li>
           <li className="nav-item dropdown">
-            <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Menopausia
             </Link>
             <ul className="dropdown-menu">
-              <li><Link className="dropdown-item" href="#">A</Link></li>
-              <li><Link className="dropdown-item" href="#">B</Link></li>
-              <li><Link className="dropdown-item" href="#">C</Link></li>
+              <li><Link className="dropdown-item" to="#">A</Link></li>
+              <li><Link className="dropdown-item" to="#">B</Link></li>
+              <li><Link className="dropdown-item" to="#">C</Link></li>
             </ul>
           </li>
         </ul>
