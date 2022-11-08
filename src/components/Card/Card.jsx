02c6574037fef9f9e0421d862ac282aa4c74b119
idoiafforero <idoia.forero@gmail.com>
2022-11-08 14:42:09 +0100

    Fixing Css Screens

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import"./Card.css";
 
-function Card({ image, title, description }) {
+function Card({ image, title, description, to }) {
   return (
     <div className="card" >
       <img src={image} className="card-img-top-pict" alt={title} />
       <div>
         <h3 className="card-title">{title}</h3>
         <p className="card-text" style={{textAlign: 'justify', padding: '2px 8px', maxWidth: '80%'}}>{description}</p>
-        <Link variant="card-link" to="" className="btn btn-card">
+        <Link variant="card-link" to={to} className="btn btn-card">
           Leer más
         </Link>
       </div>
--- src/screens/AboutUs/AboutUsScreen.jsx
+++ src/screens/AboutUs/AboutUsScreen.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Card from "../../components/Card/Card";
 import "./AboutUsScreen.css";
 
@@ -10,49 +11,57 @@ const AboutUs = () => {
         src="https://res.cloudinary.com/merjur/image/upload/v1667410614/matronas/rosa-elena_o6sjjj.jpg"
         alt="Elena Rivilla y Rosa Sánchez"
       />
-      <p  className='text-about-us' >
+      <p className="text-about-us">
         Comadrona. Midwife, sage femme, partera,…MATRONA; eso somos. Las
         matronas, somos los profesionales sanitarios oficialmente reconocidos y
         preparados específicamente para acompañar a las mujeres en todo su ciclo
-        vital. <br/>
-        <br/>
-        <strong>OM MATRONAS</strong>, nace en estos momentos de profundos cambios en la
-        sociedad que nos lleva a plantearnos nuevas maneras de acompañar a las
-        mujeres y sus familias; otras maneras de ser y estar. Todo en el
-        universo está vibrando, en continuo movimiento y transformación, según
-        las religiones dhármicas(como el hinduismo y el budismo), desde que
-        sucedió por primera vez, una primera vibración cósmica, que originó el 
-        <strong> OM</strong> y dio lugar a todo el universo, considerándose por eso la sílaba
-        sagrada, el sonido del que emergieron el resto de sonidos.<br/><br/>
-        Tradicionalmente en las clases de yoga, se entona el mantra <strong> OM </strong> tres veces
-        al inicio de la clase. Lo hacemos para invocar a ésa conciencia
-        infinita, para que pueda guiarnos en nuestra práctica y nos otorgue la
-        sabiduría de saber escucharnos, de saber ajustar nuestra respiración y
-        nuestro movimiento a cada propuesta, y de dar el paso a hacer lo mismo
-        de la esterilla a la vida. Cuando lo cantamos, nos sintonizamos
-        simbólica y físicamente a ese sonido, reconociendo nuestra conexión con
-        todos los seres vivos del universo y con el origen de la vida y nuestra
-        naturaleza. Por eso Patanjali dice que <strong>“OM es el camino hacia uno mismo,
-        hacia lo más profundo de nuestro ser”</strong>. <br/> <br/> Y por eso, el mantra OM forma
-        parte de nuestro nombre, por el significado que cobra para nosotras,
-        Matronas yoguinis, todo su simbolismo. Por significar un nuevo comienzo,
-        por significar el reconocimiento de lo que somos y de dónde venimos, por
-        significar la escucha y el acompañamiento sin juicios. Y porque como
-        pasa al cantar el OM desde el corazón, queremos acompañarte mujer, en
-        ese camino de conexión con lo más profundo de tu ser.
-      
-      <br/>
-      <h2 style={{textAlign:'center', marginTop:'1rem'}}>¿Qué ofrecemos?</h2>
-      <br/>
-      
-        Queremos <strong>acompañarte</strong> a ti mujer en todo tu ciclo vital. Y es que, la
-        transcendencia de nuestra labor va desde el acompañamiento en el
-        embarazo, el parto y la crianza; hasta brindarles atención integral en
-        materia de salud sexual y reproductiva en todas sus etapas. Por ello,
-        ofrecemos <strong>servicios</strong> que cubren todas estas esferas vitales desde la
-        infancia, para recibir vuestra primera luna hasta la llegada de la
-        madurez cíclica, pasando,cómo no, por la maravillosa etapa de recibir a
-        vuestras hijas e hijos.
+        vital. <br />
+        <br />
+        <strong>OM MATRONAS</strong>, nace en estos momentos de profundos
+        cambios en la sociedad que nos lleva a plantearnos nuevas maneras de
+        acompañar a las mujeres y sus familias; otras maneras de ser y estar.
+        Todo en el universo está vibrando, en continuo movimiento y
+        transformación, según las religiones dhármicas(como el hinduismo y el
+        budismo), desde que sucedió por primera vez, una primera vibración
+        cósmica, que originó el
+        <strong> OM</strong> y dio lugar a todo el universo, considerándose por
+        eso la sílaba sagrada, el sonido del que emergieron el resto de sonidos.
+        <br />
+        <br />
+        Tradicionalmente en las clases de yoga, se entona el mantra{" "}
+        <strong> OM </strong> tres veces al inicio de la clase. Lo hacemos para
+        invocar a ésa conciencia infinita, para que pueda guiarnos en nuestra
+        práctica y nos otorgue la sabiduría de saber escucharnos, de saber
+        ajustar nuestra respiración y nuestro movimiento a cada propuesta, y de
+        dar el paso a hacer lo mismo de la esterilla a la vida. Cuando lo
+        cantamos, nos sintonizamos simbólica y físicamente a ese sonido,
+        reconociendo nuestra conexión con todos los seres vivos del universo y
+        con el origen de la vida y nuestra naturaleza. Por eso Patanjali dice
+        que{" "}
+        <strong>
+          “OM es el camino hacia uno mismo, hacia lo más profundo de nuestro
+          ser”
+        </strong>
+        . <br /> <br /> Y por eso, el mantra OM forma parte de nuestro nombre,
+        por el significado que cobra para nosotras, Matronas yoguinis, todo su
+        simbolismo. Por significar un nuevo comienzo, por significar el
+        reconocimiento de lo que somos y de dónde venimos, por significar la
+        escucha y el acompañamiento sin juicios. Y porque como pasa al cantar el
+        OM desde el corazón, queremos acompañarte mujer, en ese camino de
+        conexión con lo más profundo de tu ser.
+        <br />
+        <h2 style={{ textAlign: "center", marginTop: "1rem" }}>
+          ¿Qué ofrecemos?
+        </h2>
+        <br />
+        Queremos <strong>acompañarte</strong> a ti mujer en todo tu ciclo vital.
+        Y es que, la transcendencia de nuestra labor va desde el acompañamiento
+        en el embarazo, el parto y la crianza; hasta brindarles atención
+        integral en materia de salud sexual y reproductiva en todas sus etapas.
+        Por ello, ofrecemos <strong>servicios</strong> que cubren todas estas
+        esferas vitales desde la infancia, para recibir vuestra primera luna
+        hasta la llegada de la madurez cíclica, pasando,cómo no, por la
+        maravillosa etapa de recibir a vuestras hijas e hijos.
       </p>
       <div className="cards-container row">
         <Card
@@ -64,6 +73,7 @@ const AboutUs = () => {
           //       Instructora certificada Hatha Yoga
           //       Instructora de esferodinamia aplicada al embarazo y al parto
           //       "
+          to="/aboutus/elena"
         />
 
         <Card
@@ -79,6 +89,7 @@ const AboutUs = () => {
           //       Formación canto prenatal y voz en el parto.
           //       Educadora certificada de masaje infantil y Baby Led Weaning.
           //       "
+          to="/aboutus/rosa"
         />
       </div>
     </div>
