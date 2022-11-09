a3e56d55a0ff60a58a93a31bfc75045a780ccd36
MerJP <merjuradoperez@gmail.com>
2022-11-09 01:22:25 +0100

    admins Screen css

--- src/screens/Admins/Elena/elenaScreen.jsx
+++ src/screens/Admins/Elena/elenaScreen.jsx
@@ -1,28 +1,31 @@
 import React from "react";
+import './elenaScreen.css'
 
 const Elena = () => {
   return (
-    <div>
-      <p>
-        Soy Elena, matrona enamorada de mi profesión y mamá de dos niños que son
-        los grandes maestros de mi vida. El yoga lleva vinculado a mi vida desde
-        hace más de 25 de años y ha sido esa guía interior que ha estado
-        presente en todos los momentos de mi vida, pero sobre todo, en mis
-        maternidades. Es por ello, me formé como instructora de yoga para el
-        embarazo, el postparto y la crianza. Además mi formación es continua y
-        soy asesora de lactancia materna y me formé en Medicina Tradicional
-        China aplicada al embarazo, al parto y postparto. Tuve el honor de haber
-        sido la Supervisora que abrió una de las maternidades referentes en la
-        atención al parto de la Comunidad de Madrid y tras mi paso por la
-        gestión, sigo dentro del equipo haciendo lo que más me gusta, acompañar
-        el nacimiento de un nuevo ser y me siento honrada por ser espectadora
-        privilegiada del renacer de esos padres y madres.
-      </p>
+    <div className="container admin-container" >
       <img
         className="adminPhoto"
         src="https://res.cloudinary.com/merjur/image/upload/v1667410188/matronas/Elena_nfs7uq.jpg"
         alt="Elena Rivilla"
       />
+      <div className="text-admin">
+        <p className="first-line-elena">Soy <strong>Elena</strong>, <strong>matrona</strong> enamorada de mi profesión y <strong>mamá</strong> de dos niños que son
+        los grandes maestros de mi vida.</p> <br/>
+        <p>El <strong>yoga</strong> lleva vinculado a mi vida desde
+        hace más de 25 de años y ha sido esa <strong>guía interior</strong> que ha estado
+        presente en todos los momentos de mi vida, pero sobre todo, en mis
+        <strong> maternidades</strong>. Es por ello, que me formé como instructora de yoga para el
+        <strong> embarazo</strong>, el <strong>postparto</strong> y la <strong>crianza</strong>. </p>
+        <p></p> Además mi formación es continua y
+        soy asesora de <strong>lactancia materna</strong> y me formé en <strong>Medicina Tradicional
+        China</strong> aplicada al embarazo, al parto y postparto. Tuve el honor de haber
+        sido la Supervisora que abrió una de las maternidades referentes en la
+        atención al parto de la Comunidad de Madrid y tras mi paso por la
+        gestión, sigo dentro del equipo haciendo lo que más me gusta, acompañar
+        el nacimiento de un nuevo ser y me siento honrada por ser espectadora
+        privilegiada del renacer de esos padres y madres.
+      </div>
     </div>
   );
 };
--- src/screens/Admins/Rosa/rosaScreen.jsx
+++ src/screens/Admins/Rosa/rosaScreen.jsx
@@ -1,38 +1,48 @@
 import React from "react";
+import "./rosaScreen.css";
 
 const Rosa = () => {
   return (
-    <div>
-      <p>
-        Hola! Soy Rosa, Mujer, Pareja, Madre, Matrona e Instructora de Yoga.
+    <div className="container admin-container">
+      <div className="text-admin">
+        Hola! Soy <strong>Rosa</strong>, <strong>Mujer</strong>,{" "}
+        <strong>Pareja</strong>, <strong>Madre</strong>,{" "}
+        <strong>Matrona</strong> e <strong>Instructora de Yoga</strong>. <br />
+        <br />
         Cuando soñaba con ser matrona me imaginaba en un gran hospital
         asistiendo partos rápidos e imprevistos continuos. Sin embargo, cursé la
         especialidad en un paritorio chiquitito cuidado por Matronas muy grandes
         y comenzó a formarse en mí el espíritu crítico como profesional y el
-        despertar al mundo femenino. Me di cuenta de que, nuestra mayor
-        responsabilidad era el reciclaje incansable para ofrecer a las mujeres y
-        familias información actualizada, mostrar empatía con sus procesos y
-        acompañarlas de manera consciente e individualizada. En estos casi 20
-        años de profesión, he reunido experiencia en el ámbito hospitalario y en
-        la Atención Primaria, tanto en centros públicos como privados brindando
-        atención a la mujer y a la infancia, así como docente. He complementado
-        mi camino con muchas formaciones de interés y entre todas ellas, conocí
-        las bondades de la práctica habitual del yoga con mis maternidades y
-        pasó a ser un puntal en mi vida. Tanto que decidí formarme como
-        Instructora de Yoga y especializarme en yoga prenatal, yoga postparto y
-        yoga para niños. Pero nada de lo contado se compara con lo aprendido
-        siendo madre. Soy madre de 3 maravillas que me han mostrado la grandeza
-        de los dones de ser mujer y sin duda me han llevado a ser mejor matrona.
-        Mi hijo mayor nació en el hospital y mi hija mediana y mi hijo pequeño
-        nacieron en casa. Cada embarazo, cada parto, cada lactancia y cada
-        crianza han sido distintos, porque cada hijo es distinto y va
-        regalándote aprendizajes diferentes. Me considero una mujer afortunada,
-        con un corazón que vibra feliz cuando está con su familia y sus amigas,
-        amante del respeto a la infancia y sus tiempos lentos y que jamás dice
-        que no a una palmerita de chocolate.
-      </p>
+        despertar al <strong>mundo femenino</strong>. Me di cuenta de que,
+        nuestra mayor responsabilidad era el reciclaje incansable para ofrecer a
+        las mujeres y familias <strong>información actualizada</strong>, mostrar{" "}
+        <strong>empatía</strong> con sus procesos y acompañarlas de manera
+        consciente e individualizada. <br />
+        En estos casi 20 años de profesión, he reunido experiencia en el ámbito
+        hospitalario y en la Atención Primaria, tanto en centros públicos como
+        privados brindando atención a la mujer y a la infancia, así como
+        docente. He complementado mi camino con muchas formaciones de interés y
+        entre todas ellas, conocí las bondades de la práctica habitual del{" "}
+        <strong>yoga</strong> con mis maternidades y pasó a ser un puntal en mi
+        vida. <br /> Tanto que decidí formarme como Instructora de Yoga y
+        especializarme en <strong>yoga prenatal</strong>,{" "}
+        <strong> yoga postparto</strong> y<strong> yoga para niños</strong>.
+        <br /> Pero nada de lo contado se compara con lo aprendido siendo{" "}
+        <strong>madre</strong>. Soy madre de 3 maravillas que me han mostrado la
+        grandeza de los dones de ser mujer y sin duda me han llevado a ser mejor
+        matrona. Mi hijo mayor nació en el hospital y mi hija mediana y mi hijo
+        pequeño
+        <strong> nacieron en casa</strong>.
+        <br />
+        Cada embarazo, cada parto, cada lactancia y cada crianza han sido
+        distintos, porque cada hijo es distinto y va regalándote aprendizajes
+        diferentes. Me considero una mujer afortunada, con un corazón que vibra
+        feliz cuando está con su familia y sus amigas, amante del respeto a la
+        infancia y sus tiempos lentos y que jamás dice que no a una palmerita de
+        chocolate.
+      </div>
       <img
-        className="adminPhoto"
+        className="adminPhoto rosa-photo"
         src="https://res.cloudinary.com/merjur/image/upload/v1667410184/matronas/Rosa_fil5fs.jpg"
         alt="Rosa Sánchez"
       />
