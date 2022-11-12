6d60d161934c861b6a6fe94a05106f3aa7a3ca25
MerJP <105247712+merJur@users.noreply.github.com>
2022-11-12 12:18:50 +0100

    Update BaseService.js

--- src/services/BaseService.js
+++ src/services/BaseService.js
@@ -3,7 +3,7 @@ import { getAccessToken, logout } from "../store/AccessTokenStore";
 
 const createHttp = (useAccessToken = false) => {
   const http = axios.create({
-    baseURL: "https://misty-jersey-ox.cyclic.app/api/¨
+    baseURL: "https://misty-jersey-ox.cyclic.app/api/"
   });
 
   // interceptors response / request
